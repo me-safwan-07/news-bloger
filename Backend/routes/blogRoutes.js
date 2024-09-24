1c0df7e91ad318683bef361dcd42d33c569365fe
me-safwan-07 <mesafwan07@gmail.com>
2024-09-24 23:43:33 +0530

    updated the blogRoutes file

--- Backend/routes/blogRoutes.js
+++ Backend/routes/blogRoutes.js
@@ -1,44 +1,12 @@
-const express = require('express');
+import express from 'express';
+import { create, getBlogs, deleteBlog, updateBlog} from '../controllers/blogController.js';
+
 const router = express.Router();
-const Blog = require('../models/Blog')
-const { incrementViewCount, incrementLikeCount } = require('../controllers/blogController');
 
 // Route to get all blogs
-router.get('/blogs', async (req, res) => {
-  try {
-    const blogs = await Blog.find();
-    res.status(200).json(blogs);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching blogs' });
-  }
-});
+router.post('/create', create);
+router.get('/get', getBlogs);
+router.delete('/delete/:id', deleteBlog);
+router.put('/update/:id', updateBlog);
 
-// Route to get a single blog by id
-router.get('/blogs/:id', async (req, res) => {
-  try {
-    const blog = await Blog.findById(req.params.id);
-    res.status(200).json(blog);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching blog' });
-  }
-});
-
-// Route to create a blog (admin only)
-router.post('/blogs', async (req, res) => {
-  const { title, content, author } = req.body;
-  try {
-    const newBlog = new Blog({ title, content, author });
-    await newBlog.save();
-    res.status(201).json(newBlog);
-  } catch (error) {
-    res.status(500).json({ message: 'Error creating blog' });
-  }
-});
-
-// Route to update blog views
-router.patch('/blogs/:id/view', incrementViewCount);
-
-// Route to update blog likes
-router.patch('/blogs/:id/like', incrementLikeCount);
-
-module.exports = router;
+export default router;
