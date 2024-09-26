128a6dab313ca92c4831844bb99bb9475ed024e1
me-safwan-07 <mesafwan07@gmail.com>
2024-09-26 23:40:56 +0530

    basic changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
+import Home from './pages/Home';
 // import TextEditor from './components/TextEditor';
 import "./App.css";
 import CreateBlog from './pages/CreateBlog';
