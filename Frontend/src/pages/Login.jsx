862ae29db021084d65096f93c2677a3e753d69d0
me-safwan-07 <mesafwan07@gmail.com>
2024-09-29 18:53:01 +0530

    created the Login page

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,42 @@
+// pages/Login.js
+import React, { useState, useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+export function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { login, isLoggedIn } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await login(email, password);
+        // Check if logged in and then navigate
+        if (isLoggedIn) {
+            navigate('/dashboard');
+        }
+    };
+
+    return (
+        <div className="">
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required 
+                />
+                <input 
+                    type="password" // Use type "password" for security
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button type='submit'>Login</button>
+            </form>
+        </div>
+    );
+}
